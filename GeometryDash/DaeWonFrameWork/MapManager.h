daddc0ba267ab28fef91464225f05ecdae204e15
AACHANJINAA <ckswlsdl0916@naver.com>
2025-11-07 00:57:07 +0900

    MapManager, UIManager, TimerManager 선언 추가 및 필터 수정

---
+++ GeometryDash/DaeWonFrameWork/MapManager.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "Singleton.h"
+
+class MapManager : public Singleton<MapManager>
+{
+public:
+	MapManager();
+	~MapManager();
+
+public:
+	void GetInstance();
+	void DestroyInstance();
+
+	void MapShow();
+	//void MapLoad();
+	void MapStage();
+
+};
+
---
+++ GeometryDash/DaeWonFrameWork/TimerManager.cpp
@@ -0,0 +1,39 @@
+#include "TimerManager.h"
+
+TimerManager::TimerManager()
+{
+}
+
+TimerManager::~TimerManager()
+{
+}
+
+void TimerManager::GetInstance()
+{
+}
+
+void TimerManager::DestroyInstance()
+{
+}
+
+void TimerManager::Reset()
+{
+}
+
+void TimerManager::Tick()
+{
+}
+
+float TimerManager::GetDeltaTime() const
+{
+	
+}
+
+float TimerManager::GetFPS() const
+{
+	
+}
+
+float TimerManager::GetTotalTime() const
+{
+}
