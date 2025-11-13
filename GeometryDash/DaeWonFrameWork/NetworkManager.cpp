d65a7e2deeab64d1f0324199d1b9aab9f57ce06f
AACHANJINAA <ckswlsdl0916@naver.com>
2025-11-13 17:28:46 +0900

    initialize_Client에 논블로킹 소켓을 써야하는이유, sendPacket, Shutdown 정의 추가
    initialize_Client에 논블로킹 소켓을 써야하는이유,
    sendPacket, Shutdown 정의 추가

--- GeometryDash/DaeWonFrameWork/NetworkManager.cpp
+++ GeometryDash/DaeWonFrameWork/NetworkManager.cpp
@@ -14,7 +14,7 @@ NetworkManager::NetworkManager()
 		// 에러	
 	}
 
-	_packetManager = ClientPacketManager::GetInstance();
+	_packetManager = ClientPacketManager::Instance();
 }
 
 NetworkManager::~NetworkManager()
@@ -36,7 +36,7 @@ bool NetworkManager::initialize_Client(const char* ip, unsigned short port)
 	_serverSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 	if (_serverSocket == INVALID_SOCKET)
 	{
-		// 에러
+		// TODO: 소켓 생성 실패	
 		return false;
 	}
 
@@ -47,7 +47,7 @@ bool NetworkManager::initialize_Client(const char* ip, unsigned short port)
 	inet_pton(AF_INET, ip, &server_address.sin_addr);
 
 	// 서버 접속
-	if (connect(_serverSocket, (sockaddr*)&server_address, SOCKET_ERROR == sizeof(server_address))) 
+	if (connect(_serverSocket, (sockaddr*)&server_address, sizeof(server_address)) == SOCKET_ERROR)
 	{
 		closesocket(_serverSocket);
 		_serverSocket = INVALID_SOCKET;
@@ -67,8 +67,12 @@ bool NetworkManager::initialize_Client(const char* ip, unsigned short port)
 	return true;
 }
 
-void NetworkManager::sendPacket()
+void NetworkManager::sendPacket(char* buffer, int size)
 {
+	if (_serverSocket != INVALID_SOCKET)
+	{
+		send(_serverSocket, buffer, size, 0);
+	}
 }
 
 void NetworkManager::recvPacket()
@@ -77,8 +81,47 @@ void NetworkManager::recvPacket()
 
 void NetworkManager::updatePacket()
 {
+	if (_serverSocket == INVALID_SOCKET) return;
+
+	// 1. _recvBuffer의 남은 공간에 데이터를 수신 시도
+	int bytesRecv = recv(_serverSocket,
+		_recvBuffer + _recvBufferSize,   // 버퍼의 빈 공간 시작 위치
+		4096 - _recvBufferSize,          // 버퍼의 남은 크기
+		0);
+
+	if (bytesRecv > 0)
+	{
+		// 2. 데이터가 왔으면, 버퍼에 쌓인 크기를 늘림
+		_recvBufferSize += bytesRecv;
+
+		// 3. 버퍼에 완성된 패킷이 있는지 파싱
+		processPacket();
+	}
+	else if (bytesRecv == 0)
+	{
+		// 4. (접속 종료) 서버가 접속을 끊음
+		shutdown();
+	}
+	else // bytesRecv == SOCKET_ERROR
+	{
+		int error = WSAGetLastError();
+		if (error == WSAEWOULDBLOCK)
+		{
+			// 5. (정상) 데이터가 없는 것뿐. 아무것도 하지 않음.
+		}
+		else
+		{
+			// 6. (오류) 실제 오류 발생. 접속 종료.
+			shutdown();
+		}
+	}
 }
 
 void NetworkManager::shutdown()
 {
+	if (_serverSocket != INVALID_SOCKET)
+	{
+		closesocket(_serverSocket);
+		_serverSocket = INVALID_SOCKET;
+	}
 }
